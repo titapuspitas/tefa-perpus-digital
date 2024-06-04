0114f42adb4f58d100ddd529ce1f60cce93c2f85
titapuspitas <141605952+titapuspitas@users.noreply.github.com>
2024-06-04 10:57:21 +0700

    Add files via upload

--- pages/buku/index.vue
+++ pages/buku/index.vue
@@ -5,20 +5,16 @@ import { NuxtLink } from '#build/components';
       <div class="col-lg-12 -flex justify-content-around">
         <div class="my-5">
           <form @submit.prevent="getBuku">
-            <input 
-            v-model="keyword" 
-            type="search" 
-            class="form-control rounded-5" 
-            placeholder="Mau baca apa hari ini?">
+            <input v-model="keyword" type="search" class="form-control rounded-5" placeholder="Mau baca apa hari ini?" />
           </form>
         </div>
-        <div class="my-4 text-muted">menampilkan 30 dari 30</div>
+        <div class="my-4 text-muted">menampilkan {{ buku?.length }} dari {{ totalBuku }}</div>
         <div class="row justify-content-evenly">
-          <div v-for="(buku,i) in buku" :key="i" class="col-lg-2">
+          <div v-for="(buku, i) in buku" :key="i" class="col-lg-2">
             <NuxtLink :to="`/buku/${buku.id}`">
               <div class="card mb-3">
                 <div class="card-body">
-                  <img :src="buku.cover" class="cover" :alt="buku.judul">
+                  <img :src="buku.cover" class="cover" :alt="buku.judul" />
                 </div>
               </div>
             </NuxtLink>
@@ -33,27 +29,30 @@ import { NuxtLink } from '#build/components';
 </template>
 
 <script setup>
-const supabase = useSupabaseClient()
+const supabase = useSupabaseClient();
 
-const buku = ref([])
+const buku = ref([]);
+const totalBuku = ref(0);
 
 const getBuku = async () => {
-  const { data, error } = await supabase
-  .from('buku')
-  .select('*, kategori(*)')
-  .ilike("judul", `%${keyword.value}%`);
-  if(data) buku.value = data;
-}
+  const { data, error } = await supabase.from("buku").select("*, kategori(*)").ilike("judul", `%${keyword.value}%`);
+  if (data) buku.value = data;
+};
+const getTotalBuku = async () => {
+  const { count, error } = await supabase.from("buku").select("*, kategori(*)", { count: 'exact', head: true });
+  if (count) totalBuku.value = count;
+};
 
 onMounted(() => {
-  getBuku()
+  getBuku();
+  getTotalBuku();
 });
 
 const keyword = ref("");
 </script>
 
 <style scoped>
-.card-body{
+.card-body {
   width: 100%;
   height: 400px;
   padding: 0;
@@ -64,10 +63,10 @@ const keyword = ref("");
   object-fit: cover;
   object-position: 0 30;
 }
-.btn{
+.btn {
   background-color: #ffd0d0f2;
 }
-.form-control{
+.form-control {
   background-color: #ffd0d0f2;
 }
 </style>
