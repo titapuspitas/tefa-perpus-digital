0e01850dac9167ecaeeb341e3035b18aa07306e6
titapuspitas <141605952+titapuspitas@users.noreply.github.com>
2024-11-21 12:57:40 +0700

    Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -1,33 +1,49 @@
 <template>
-  <div class="header">
-    <div class="h-img">
-      <img src="~/assets/img/logo-smkn4.png" alt="logo smkn 4">
+  <nav class="navbar fixed">
+    <div class="container-fluid d-flex align-items-center justify-content-center">
+      <span class="navbar-brand mb-0 h1 text-center">
+        Selamat Datang Di Tabungan {{ judul }}
+      </span>
     </div>
-    <div class="h-text">
-    <div class="t-judul">
-      <h1>PERPUSTAKAAN DIGITAL</h1>
-      <address>
-        SMKN 4 Tasikmalaya
-        <p>jln. Depok, sukamenak, purbaratu</p>
-      </address>
-    </div>
-  </div>
-  </div>
+  </nav>
 </template>
+
+<script setup>
+defineProps({
+  judul: String
+})
+</script>
+
 <style scoped>
-.header{
-  height: 150px;
-  display: flex;
-  align-items: center;
-  background-color: #0000FF;
-  color: #ffffff;
+.navbar {
+  background: #1A9EA7;
+  height: 15vh;
 }
-.h-img >  img {
-  width: 120px;
-  height: 120px;
-  margin-left: 50px;
+
+.navbar-brand {
+  color: white;
+  font-size: 40px;
+  font-weight: bold;
+  text-align: center;
 }
-.t-judul{
-  margin-left: 40px;
+
+@media (max-width: 768px) {
+  .navbar {
+    height: 10vh; /* Kurangi tinggi navbar untuk layar kecil */
+  }
+
+  .navbar-brand {
+    font-size: 24px; /* Ukuran font lebih kecil */
+  }
+}
+
+@media (max-width: 576px) {
+  .navbar {
+    height: 8vh; /* Tinggi lebih kecil untuk layar HP */
+  }
+
+  .navbar-brand {
+    font-size: 18px; /* Ukuran font lebih kecil lagi */
+  }
 }
 </style>
